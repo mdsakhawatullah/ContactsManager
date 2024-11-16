e9564f0b596ad51b1a088000257f716474425c75
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-11-16 10:44:52 +0600

    Add project files.

---
+++ Test/CountriesServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+    }
+}
