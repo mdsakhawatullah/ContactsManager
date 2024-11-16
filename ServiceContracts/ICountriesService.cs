a12c545e82c00c63acf7abd9691419437477cd3d
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-11-16 10:44:52 +0600

    Testcases for AddCountryAdd method

---
+++ ServiceContracts/ICountriesService.cs
@@ -0,0 +1,17 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Business logic for manupulating Country entity
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Adds a Country object to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to add</param>
+        /// <returns>Returns the Country object after adding it</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+    }
+}
