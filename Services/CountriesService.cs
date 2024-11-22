30cf9d6d4091180f5203fe25069c26d0db0b38b6
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-11-22 09:03:22 +0600

    AddCountry method implementation

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,14 +1,53 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
 
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            if(_countries.Where(country => country.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("This Country Name is already exists.");
+            }
+
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate CountryId
+            country.CountryId = new Guid();
+
+            //finally add to the list
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
         }
+
+
+
+
+
+       
     }
 }
