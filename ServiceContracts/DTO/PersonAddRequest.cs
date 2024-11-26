1e557fdcc3be058a1b1a9add77de0b6fed836cb0
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-11-26 08:42:54 +0600

    DTO PersonAddRequest, PersonResponse added

---
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using ServiceContracts.DTO.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    public class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+
+        /// <summary>
+        /// Converts PersonAddRequest to Person object
+        /// </summary>
+        /// <returns></returns>
+        public Person ToPerson()
+        {
+            return new Person() {
+                      PersonName = PersonName, Email = Email, DateOfBirth = DateOfBirth,
+                      Gender = Gender.ToString(), Address = Address, CountryID = CountryID,
+                      ReceiveNewsLetters = ReceiveNewsLetters
+                     };
+        }
+    }
+}
---
+++ ServiceContracts/DTO/PersonResponse.cs
@@ -0,0 +1,52 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class that represent as ReturnType
+    /// </summary>
+    public class PersonResponse
+    {
+        public Guid PersonID { get; set; }
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Country { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+        public double? Age { get; set; }
+
+    }
+    public static class PersonExtensions
+    {
+        /// <summary>
+        /// Extension method for convert Person object To PersonResponse class
+        /// </summary>
+        /// <param name="person">Person object to convert</param>
+        /// <returns>Returns the PersonResponse object</returns>
+        public static PersonResponse ToPersonResponse(this Person person)
+        {
+            
+            return new PersonResponse()
+            {
+                PersonID = person.PersonID,
+                PersonName = person.PersonName,
+                Email = person.Email,
+                DateOfBirth = person.DateOfBirth,
+                ReceiveNewsLetters = person.ReceiveNewsLetters,
+                Address = person.Address,
+                CountryID = person.CountryID,
+                Gender = person.Gender,
+                Age = (person.DateOfBirth != null) ? Math.Round((DateTime.Now - person.DateOfBirth.Value).TotalDays / 365.25) : null
+            };
+        }
+    }
+}
